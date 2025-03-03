1af43861c7191018bd631ce59f4299f8a41787a5
gouni <gounimahesh9000@gmail.com>
2025-03-04 03:30:55 +0530

    here we need to write the role based auth

--- src/main/java/com/neoteric/HotelManningMahiDev/registerApi/controller/UserController.java
+++ src/main/java/com/neoteric/HotelManningMahiDev/registerApi/controller/UserController.java
@@ -1,44 +1,44 @@
-package com.neoteric.HotelManningMahiDev.registerApi.controller;
-
-
-
-import com.neoteric.HotelManningMahiDev.exceptions.ErrorResponse;
-import com.neoteric.HotelManningMahiDev.registerApi.modelOrDto.Login;
-import com.neoteric.HotelManningMahiDev.registerApi.modelOrDto.User;
-import com.neoteric.HotelManningMahiDev.registerApi.service.UserService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.logging.Logger;
-
-
-
-@RestController
-@RequestMapping("/api/auth")
-public class UserController {
-    private static final Logger logger = Logger.getLogger(UserController.class.getName());
-
-
-    private UserService userService;
-
-    public UserController( UserService userService){
-        this.userService=userService;
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<ErrorResponse> register(@RequestBody @Valid User user){
-        logger.info(" sign up user received"+user);
-        userService.register(user);
-        return new  ResponseEntity<>(HttpStatus.ACCEPTED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<ErrorResponse> authenticateUser(@RequestBody @Valid Login login){
-        logger.info("login in the controller"+login);
-        userService.login(login);
-        return new ResponseEntity<>( HttpStatus.OK);
-    }
-
-}
+//package com.neoteric.HotelManningMahiDev.registerApi.controller;
+//
+//
+//
+//import com.neoteric.HotelManningMahiDev.exceptions.ErrorResponse;
+//import com.neoteric.HotelManningMahiDev.registerApi.modelOrDto.Login;
+//import com.neoteric.HotelManningMahiDev.registerApi.modelOrDto.User;
+//import com.neoteric.HotelManningMahiDev.registerApi.service.UserService;
+//import jakarta.validation.Valid;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.logging.Logger;
+//
+//
+//
+//@RestController
+//@RequestMapping("/api/auth")
+//public class UserController {
+//    private static final Logger logger = Logger.getLogger(UserController.class.getName());
+//
+//
+//    private UserService userService;
+//
+//    public UserController( UserService userService){
+//        this.userService=userService;
+//    }
+//
+//    @PostMapping("/signup")
+//    public ResponseEntity<ErrorResponse> register(@RequestBody @Valid User user){
+//        logger.info(" sign up user received"+user);
+//        userService.register(user);
+//        return new  ResponseEntity<>(HttpStatus.ACCEPTED);
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<ErrorResponse> authenticateUser(@RequestBody @Valid Login login){
+//        logger.info("login in the controller"+login);
+//        userService.login(login);
+//        return new ResponseEntity<>( HttpStatus.OK);
+//    }
+//
+//}
